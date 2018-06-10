838828262c6911517c8a0e0b221b0eaf8be19fe0
Federico Santamorena <federico@santamorena.me>
2018-06-10 02:23:47 +0200

    added single header for easier including

---
+++ include/cglmath.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "cglmath/float.h"
+
+#include "cglmath/vector2i_type.h"
+#include "cglmath/vector2f_type.h"
+
+#include "cglmath/vector3i_type.h"
+#include "cglmath/vector3f_type.h"
+#include "cglmath/vector4f_type.h"
+
+#include "cglmath/vector2f.h"
+#include "cglmath/vector3f.h"
+#include "cglmath/vector4f.h"
+
+#include "cglmath/quaternion_type.h"
+#include "cglmath/quaternion.h"
+
+#include "cglmath/transform_type.h"
+#include "cglmath/transform.h"
+
+#include "cglmath/matrix4f_type.h"
+#include "cglmath/matrix4f.h"
